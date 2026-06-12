2103ee5fd20de170dad85a293d956626e13ba705
tu_usuario <262179456+owlinx@users.noreply.github.com>
2026-06-12 12:52:00 -0600

    version 1

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ if 'df_experiment_results' not in st.session_state:
 
 st.header('Lanzar una moneda')
 
-<<<<<<< HEAD
 # contenedor para el gráfico
 chart_placeholder = st.empty()
 
@@ -37,14 +36,11 @@ def toss_coin(n):
 
     return mean
 
-=======
->>>>>>> aacec020bd50afe24c8ddb2bae0f5da70ce2d7c3
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
-<<<<<<< HEAD
     st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
     st.session_state['df_experiment_results'] = pd.concat([
@@ -58,7 +54,3 @@ if start_button:
     st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
 
 st.write(st.session_state['df_experiment_results'])
-=======
-
-st.write('Esta aplicación aún no es funcional. En construcción.')
->>>>>>> aacec020bd50afe24c8ddb2bae0f5da70ce2d7c3
